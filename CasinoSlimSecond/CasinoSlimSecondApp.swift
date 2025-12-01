7306f91e01694ba4ace3f7c56a9cec405931d99c
keshew <artyom.korotckov2017@yandex.ru>
2025-12-01 14:48:02 +0500

    Initial Commit

---
+++ CasinoSlimSecond/CasinoSlimSecondApp.swift
@@ -0,0 +1,17 @@
+//
+//  CasinoSlimSecondApp.swift
+//  CasinoSlimSecond
+//
+//  Created by Артём Коротков on 01.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct CasinoSlimSecondApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
